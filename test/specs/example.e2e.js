7437bbac960f322f34537801b9a892f4fcb0882d
TanyaBrant <tanya.brant.gsuite@gmail.com>
2021-03-19 23:24:53 -0500

    Page Object

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,18 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
+import ProfilePage from '../pageobjects/profile.page';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+describe('Auth', () => {
+    it('User logs in with valid credentials', () => {
         LoginPage.open();
+        LoginPage.setLogin('tanya.brant@me.com');
+        LoginPage.setPassword('123123');
+        LoginPage.clickSubmitButton();
+        ProfilePage.isOpen();
 
-        LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+        // LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        // expect(SecurePage.flashAlert).toBeExisting();
+        // expect(SecurePage.flashAlert).toHaveTextContaining(
+        //     'You logged into a secure area!');
     });
 });
 
